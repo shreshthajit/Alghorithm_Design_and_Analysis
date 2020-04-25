c5b126be3c9c40a8a30c2458cb1d81a21c4fc7fd
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2020-04-25 20:46:55 +0600

    Update Z_algho.cpp

--- Z_Algho_problem/Z_algho.cpp
+++ Z_Algho_problem/Z_algho.cpp
@@ -1,7 +1,5 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
 vector<int> calculateZ(string inp)
 {
     int len = inp.length();
@@ -23,15 +21,14 @@ vector<int> calculateZ(string inp)
         else
         {
             int k1 = k - left;
-            // inside the Z-Box
+             
             if(Z[k1] < right-k+1)
             {
                 Z[k] = Z[k1];
-                //if the value is inside the Z-box then copy it
+                
             }
             else
             {
-                // otherwise do more comparisons
                 left = k;
                 while(right < len && inp[right] == inp[right-left])
                 {
@@ -43,7 +40,6 @@ vector<int> calculateZ(string inp)
         }
     }
     return Z;
-    // Z[i] denotes the length of the longest substring starting from i which is also the prefix of the string.
 }
 int main()
 {
