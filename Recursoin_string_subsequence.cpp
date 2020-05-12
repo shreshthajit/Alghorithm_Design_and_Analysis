ee27eabee99881b907f57b0441728d157225194e
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2020-05-12 16:50:41 +0600

    Add files via upload

---
+++ Recursoin_string_subsequence.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void calculate(string str,int pos=0,string temp="")
+{
+    if(pos==str.length())
+    {
+        cout<<temp<<endl;
+        return;
+    }
+
+    calculate(str,pos+1,temp+str[pos]);
+    calculate(str,pos+1,temp);
+}
+
+
+
+
+
+
+int main()
+{
+    string s;
+
+    cin>>s;
+
+    calculate(s);
+
+}
