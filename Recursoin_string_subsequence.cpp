34840100a0f97806bc5452a6fe936acf4387c001
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2020-05-12 16:51:12 +0600

    Update Recursoin_string_subsequence.cpp

--- Recursoin_string_subsequence.cpp
+++ Recursoin_string_subsequence.cpp
@@ -9,22 +9,12 @@ void calculate(string str,int pos=0,string temp="")
         cout<<temp<<endl;
         return;
     }
-
     calculate(str,pos+1,temp+str[pos]);
     calculate(str,pos+1,temp);
 }
-
-
-
-
-
-
 int main()
 {
     string s;
-
     cin>>s;
-
     calculate(s);
-
 }
