1df4dd2ed0582a64401e596e17ee4cbdcb99b5b3
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2020-05-13 20:49:55 +0600

    Update Backtracking_possible_path_ina_maze.cpp

--- Backtracking_possible_path_ina_maze.cpp
+++ Backtracking_possible_path_ina_maze.cpp
@@ -6,12 +6,7 @@ using namespace std;
 bool vis[100][100];
 
 int path=0;
-
-
-
-
-
-
+ 
 int go(vector<vector<int> >v,int sx,int sy,int dx,int dy,int r,int c)
 {
     if(sx<0 || sy<0 || sx>=r || sy>=c || v[sx][sy]==0 || vis[sx][sy]==true)
@@ -21,8 +16,6 @@ int go(vector<vector<int> >v,int sx,int sy,int dx,int dy,int r,int c)
 
     if(sx==dx && sy==dy)
         path++;
-
-
     go(v,sx,sy+1,dx,dy,r,c);
     go(v,sx,sy-1,dx,dy,r,c);
     go(v,sx+1,sy,dx,dy,r,c);
@@ -30,9 +23,6 @@ int go(vector<vector<int> >v,int sx,int sy,int dx,int dy,int r,int c)
 
     vis[sx][sy]=false;
 }
-
-
-
 int main()
 {
     int r,c;
@@ -47,21 +37,7 @@ int main()
         for(int j=0; j<c; j++)
             cin>>v[i][j];
     }
-
     memset(vis,false,sizeof(vis));
-
     go(v,0,0,r-1,c-1,r,c);
-
-
     cout<<path<<endl;
-
-
-
-
-
-
-
-
-
-
 }
