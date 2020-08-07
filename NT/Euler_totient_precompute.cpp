859773a7c9817bdad7ea47faaac9468bfbd791d8
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2020-08-08 04:36:39 +0600

    Add files via upload

---
+++ NT/Euler_totient_precompute.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define mx 100005
+int phi[mx];
+
+void euler_totient() {
+    for(int i=0; i<mx; i++) phi[i]=i;
+    for(int i=2; i<mx; i++) {
+        if(phi[i]==i) {
+            for(int j=i; j<mx; j+=i)
+                phi[j]-=(phi[j]/i);
+        }
+    }
+}
+int main(){
+
+  euler_totient();
+  for(int i=0;i<mx;i++)
+  cout<<phi[i]<<" ";
+
+}
