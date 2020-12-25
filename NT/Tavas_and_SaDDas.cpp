147c189603d9e1b51a125bcaf25cb9275861d531
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2020-12-25 20:30:09 +0600

    Add files via upload

---
+++ NT/Tavas_and_SaDDas.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod (int) 998244353
+#define MOD (int) 1e9+7
+#define X 1001100011100001111ll
+#define all(a) a.begin(),a.end()
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define loop(i,a,b) for (int i = a; i < b; i++)
+#define bloop(i,a,b) for (int i = a ; i>=b;i--)
+#define tc(t) int t; cin >> t; while (t--)
+#define int long long
+#define ll long long
+#define pb emplace_back
+#define fio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define in(x) scanf("%d", &x)
+#define rr return 0
+#define prec(n) fixed<<setprecision(n)
+#define maxpq priority_queue<int>
+#define minpq priority_queue<int, vector<int>, greater<int> >
+#define inf (int)(1e18)
+#define ini(a, i) memset(a, i, sizeof(a))
+#define vi vector<int>
+#define fi first
+#define se second
+#define endl "\n"
+#define ii pair<int, int>
+#define vii vector<ii>
+#define sz(s) s.size()
+#define bits(n) __builtin_popcount(n)
+#define mx 10000
+
+signed main()
+{
+
+    string s;
+    cin>>s;
+
+    ll ans=0;
+    for(int i=0;i<s.length();i++){
+        ans=ans*2;
+        if(s[i]=='7')
+            ans++;
+    }
+    cout<<ans+pow(2,s.length())-1<<endl;
+}
+
+
