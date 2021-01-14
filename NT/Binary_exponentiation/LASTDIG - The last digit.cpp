578c2c6f52fb311ef2d68e90fa9a53c2dc083c13
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2021-01-14 16:34:59 +0600

    Update LASTDIG - The last digit.cpp

--- NT/Binary_exponentiation/LASTDIG - The last digit.cpp
+++ NT/Binary_exponentiation/LASTDIG - The last digit.cpp
@@ -1,1 +1,33 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll long long
+
+long long  binpow(long long  x, long  n)
+{
+    long long  res = 1 ;
+    while( n > 0)
+    {
+        if( n & 1 ) {
+                res = ( res * x ) ;
+                res=res%10;
+        }
+        n=n/2;
+        x = ( x * x );
+        x=x%10;
+    }
+    return res ;
+}
+signed main()
+{
+    ll a,b,md;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        cout<<binpow(a,b)<<endl;
+    }
+}
+
