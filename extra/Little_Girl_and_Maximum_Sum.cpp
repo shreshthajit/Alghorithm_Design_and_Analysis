019b5dd25cd796294d9614f7554e38d7b8b1fe03
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2021-04-15 23:28:08 +0600

    Update Little_Girl_and_Maximum_Sum.cpp

--- extra/Little_Girl_and_Maximum_Sum.cpp
+++ extra/Little_Girl_and_Maximum_Sum.cpp
@@ -64,7 +64,7 @@ cum query--->1 1 0 0 1 1
 
     
     
-    
+     
     
  #include<bits/stdc++.h>
 using namespace std;
@@ -109,3 +109,87 @@ int main()
 
     cout<<sum;
 }
+---------------------------------------------------------------------------------------------------------------------
+  
+  
+  
+  
+  
+  there is an another idea using segment tree and the idea is same as the previously discussed idea.
+  #https://www.topcoder.com/thrive/articles/Binary%20Indexed%20Trees
+    
+#include<bits/stdc++.h>
+#define MAX_N 300001
+using namespace std;
+
+typedef long long lld;
+
+int n,q,x,y;
+lld ret = 0LL;
+
+lld niz[MAX_N];
+lld new_niz[MAX_N];
+
+lld tree[MAX_N];
+void update(int x, lld val)
+{
+    while (x <= n)
+    {
+        tree[x] += val;
+        x += (x & -x);
+    }
+}
+lld read(int x)
+{
+    int sum=0;
+    while (x>0)
+    {
+        sum+=tree[x];
+        x -= (x & -x);
+    }
+    return sum;
+}
+
+int main()
+{
+    scanf("%d%d",&n,&q);
+    for (int i=0;i<n;i++) scanf("%I64d",&niz[i]);
+    sort(niz,niz+n);
+    while (q--)
+    {
+        scanf("%d%d",&x,&y);
+        update(x,1);
+        update(y+1,-1);
+    }
+    for (int i=1;i<=n;i++)
+    {
+        new_niz[i-1] = read(i);
+        //printf("%d. %d\n",i, new_niz[i-1]);
+    }
+    sort(new_niz,new_niz+n);
+    for (int i=n-1;i>=0;i--)
+    {
+        ret += niz[i] * new_niz[i];
+    }
+    cout << ret << endl;
+    return 0;
+} 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
