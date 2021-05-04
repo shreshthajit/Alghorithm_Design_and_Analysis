779a9a56afbe4faa2f906405826991c58cedbc19
jani na <43321488+shreshthajit@users.noreply.github.com>
2021-05-04 16:28:02 +0600

    Update Is it rated_2.cpp

--- string/Is it rated_2.cpp
+++ string/Is it rated_2.cpp
@@ -1,5 +1,5 @@
-///https://en.cppreference.com/w/cpp/io/c/fflush?fbclid=IwAR0DfeX_iVbIei-FldPlZZMnybdM9iq9Hokl393olZnRy6cKFKbTdniZz6E
-
+#https://en.cppreference.com/w/cpp/io/c/fflush?fbclid=IwAR0DfeX_iVbIei-FldPlZZMnybdM9iq9Hokl393olZnRy6cKFKbTdniZz6E
+#https://codeforces.com/blog/entry/89260?fbclid=IwAR14LzQrk5zrZ6lXNugumJ81r42ynY09OCFzLq_EqkohelQgMIV7V9LceRE
 #include<bits/stdc++.h>
 using namespace std;
 
