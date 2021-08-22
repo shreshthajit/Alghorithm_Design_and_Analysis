224cbe84e10586754b5b71f3b5e638d8dcf02dc1
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2021-08-22 18:28:37 +0600

    Update array_diiferentiation.cpp

--- Bitmask/Codeforces/array_diiferentiation.cpp
+++ Bitmask/Codeforces/array_diiferentiation.cpp
@@ -1,1 +1,53 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+void solve(vector<int>& nums)
+{
+
+    map<int,int>mp;
+    int siz=nums.size();
+    int subsetNum=1<<siz;
+    for(int mask=0; mask<subsetNum; mask++)
+    {
+         int sum=0;
+        for(int i=0; i<siz; i++)
+        {
+            if(mask&(1<<i))
+            {
+                sum+=nums[i];
+            }
+        }
+        mp[sum]++;
+    }
+    for(auto it:mp)
+    {
+        if(it.second>1){
+            cout<<"YES"<<endl;
+        return;
+        }
+
+    }
+    cout<<"NO"<<endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>nums(n);
+        for(int i=0;i<n;i++)
+            cin>>nums[i];
+
+        solve(nums);
+
+
+    }
+}
+/*
+
+
+
+*/
