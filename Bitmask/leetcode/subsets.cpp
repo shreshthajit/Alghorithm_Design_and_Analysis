270289d7bddbbbde70598c7e2a2da211dac8dbfa
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2021-08-22 18:04:48 +0600

    Update subsets.cpp

--- Bitmask/leetcode/subsets.cpp
+++ Bitmask/leetcode/subsets.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>>ans;
+        int size=nums.size();
+        int subsetNum=1<<size;
+        for(int mask=0;mask<subsetNum;mask++){
+             vector<int>subset;
+            for(int i=0;i<size;i++){
+                if(mask&(1<<i)){
+                    subset.push_back(nums[i]);
+                }
+            }
+            ans.push_back(subset);
+        }
+        return ans;
+    }
+};
