73f734658b367def228d1689963594ad7b3041b7
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2022-01-27 22:16:08 +0600

    Create Longest Commom Substring.cpp

---
+++ DP/Longest Commom Substring.cpp
@@ -0,0 +1,102 @@
+ #include<bits/stdc++.h>
+using namespace std;
+#define mod (int) 998244353
+#define MOD (int) 1e9+7
+ #define all(a) a.begin(),a.end()
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define loop(i,a,b) for (int i = a; i < b; i++)
+#define bloop(i,a,b) for (int i = a ; i>=b;i--)
+#define tc(t) int t; cin >> t; while (t--)
+#define int long long
+#define ll long long
+#define pb emplace_back
+#define fio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define in(x) scanf("%d", &x)
+#define rr return 0
+#define prec(n) fixed<<setprecision(n)
+#define maxpq priority_queue<int>
+#define minpq priority_queue<int, vector<int>, greater<int> >
+#define inf (int)(1e18)
+#define ini(a, i) memset(a, i, sizeof(a))
+#define vi vector<int>
+#define fi first
+#define se second
+#define endl "\n"
+#define ii pair<int, int>
+#define vii vector<ii>
+long long  binpow(long long  x, long  n)
+{
+    x %= MOD;
+    long long  res = 1 ;
+    while( n > 0)
+    {
+        if( n & 1 ) res = ( res * x ) % MOD ;
+        ///n >>= 1 ;
+        n=n/2;
+        x = ( x * x ) % MOD;
+    }
+    return res ;
+}
+
+bool ArePair(char opening,char closing)
+{
+	if(opening == '(' && closing == ')') return true;
+	else if(opening == '{' && closing == '}') return true;
+	else if(opening == '[' && closing == ']') return true;
+	return false;
+}
+bool AreParanthesesBalanced(string exp)
+{
+	stack<char>  S;
+	for(int i =0;i<exp.length();i++)
+	{
+		if(exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+			S.push(exp[i]);
+		else if(exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+		{
+			if(S.empty() || !ArePair(S.top(),exp[i]))
+				return false;
+			else
+				S.pop();
+		}
+	}
+	return S.empty() ? true:false;
+}
+
+signed main()
+{
+    tc(t){
+         string X,Y;
+         cin>>X>>Y;
+        int m=X.length();
+        int n=Y.length();
+        int dp[m+1][n+1];
+
+        // initialization
+        for(int i=0;i<=m;i++)
+            dp[i][0]=0;   // Eg LCS of "abc" & "" = 0
+        for(int j=0;j<=n;j++)
+            dp[0][j]=0;   // Eg LCS of "" & "abc" = 0
+
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(X[i-1]==Y[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=0;                   // ONLY THIS CHANGE
+            }
+        }
+    	 int maxLen=0;              // Now finding the max element
+         for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+               maxLen=max(maxLen,dp[i][j]);
+        }
+        cout<<maxLen<<endl;
+
+}
+}
+
+
